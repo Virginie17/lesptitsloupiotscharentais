9ca2a38db8dee1468b1b031084ad35207dfeb03e
Virginie17 <niniezouk@hotmail.fr>
2025-10-22 18:20:11 +0200

    correction

--- .history/src/app/api/contact/route_20251022181926.ts
+++ .history/src/app/api/contact/route_20251022181926.ts
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.NEXT_PUBLIC_EMAIL_USER,
-    pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD,
+    pass: process.env.GMAIL_APP_PASSWORD,
   },
 })
 
